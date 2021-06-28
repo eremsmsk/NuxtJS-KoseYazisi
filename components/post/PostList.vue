bcb8a725b610a82430c8528f4521e98f9fa463c7
eremsimsek <eremsmsk@gmail.com>
2021-06-28 14:54:14 +0300

    Add: Post and About Page

---
+++ components/post/PostList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      container-fluid
+      mt-5
+      d-flex
+      flex-wrap
+      justify-content-center
+      align-items-center
+    "
+  >
+    <Post v-for="index in 10" :key="index" />
+  </div>
+</template>
+<script>
+import Post from "@/components/post/Post";
+
+export default {
+  components: {
+    Post,
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  
+  <div>
+    <PostList />
+    <About />
+  </div>
 </template>
 <style scoped>
-
 </style>
 <script>
+import PostList from "@/components/post/PostList";
+import About from "@/components/home/About";
+export default {
+  components: {
+    PostList,
+    About
+  },
+};
 </script>
