476bffea14869a505d115d0c9f21a95953a3d2c9
eremsmsk <eremsmsk@gmail.com>
2021-07-15 12:53:16 +0300

    Add: Navigation operations on header

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,21 +1,22 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-0">
-    <a class="navbar-brand" href="/">Köse Yazısı</a>
+      <nuxt-link to="/" class="navbar-brand" tag="a">
+        Köse Yazısı
+      </nuxt-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="/posts">Yazılar <span class="sr-only">(current)</span></a>
-            </li>
-
+          <nuxt-link to="/posts" tag="li" class="nav-item">
+            <a class="nav-link" href="#">Yazılar</a>
+          </nuxt-link>
         </ul>
         <ul class="navbar-nav my-2 my-lg-0">
-            <li class="nav-item mr-sm-2">
-                <a class="nav-link" href="/admin">Admin</a>
-            </li>
+          <nuxt-link to="/admin" tag="li" class="nav-item">
+            <a class="nav-item mr-sm-2" href="#">Admin</a>
+          </nuxt-link>
         </ul>
     </div>
 </nav>
