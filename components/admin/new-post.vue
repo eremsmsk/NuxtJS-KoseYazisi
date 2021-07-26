2ffb9ee132248f5d4969a6f46f4829cee6d7d1af
eremsmsk <eremsmsk@gmail.com>
2021-07-26 12:20:08 +0300

    Edit: Data from Forms Dynamically via Event Emit protect your post

--- components/admin/new-post.vue
+++ components/admin/new-post.vue
@@ -1,11 +1,17 @@
 <template>
-    <PostForm />
+  <PostForm @submit="savePost($event)"/>
 </template>
 <script>
 import PostForm from "@/components/admin/PostForm"
+
 export default {
-   components: {
-       PostForm
-   } 
+  components: {
+    PostForm
+  },
+  methods: {
+    savePost(post) {
+      console.log(post)
+    }
+  }
 }
 </script>
