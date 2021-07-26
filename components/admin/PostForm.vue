2e22aa269c07f1d6f9185e7103a1ea2463724f44
eremsmsk <eremsmsk@gmail.com>
2021-07-26 12:38:00 +0300

    Edit: Dynamically on PostForm <legend>Change</legend>

--- components/admin/PostForm.vue
+++ components/admin/PostForm.vue
@@ -2,7 +2,8 @@
   <div class="container-fluid mt-5 d-flex flex-wrap justify-content-center align-items-center">
     <form style="width: 500px" @submit.prevent="onSubmit()">
       <fieldset>
-        <legend>Yeni Köse Yazısı</legend>
+        <legend v-if="isUpdate">Köse Yazısı Düzenle</legend>
+        <legend v-else>Yeni Köse Yazısı</legend>
         <div class="form-group">
           <label>Yazar Adı</label>
           <input v-model="post.author" type="text" class="form-control" placeholder="Yazarın adını giriniz..">
@@ -19,7 +20,7 @@
           <label for="exampleTextarea">Köse Yazısı</label>
           <textarea v-model="post.text" class="form-control" rows="5"></textarea>
         </div>
-        <button class="btn btn-danger">İptal</button>
+        <button @click="$router.push('/admin')" class="btn btn-danger">İptal</button>
         <button type="submit" class="btn btn-primary">Kaydet</button>
       </fieldset>
     </form>
@@ -36,6 +37,14 @@ export default {
         author: null,
         text: null
       }
+    }
+  },
+
+  props: {
+    isUpdate: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   },
 
